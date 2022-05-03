61c1c7a65ed6cf4261700f289575ee01290d4002
jcorrea97 <jroberto.cr1@gmail.com>
2022-05-03 10:56:29 -0300

    feat(componentes): melhoria no theme builder

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -27,8 +27,6 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('resultCheckBox') resultCheckBox: HTMLElement;
   @ViewChild('resultSwitch') resultSwitch: HTMLElement;
 
-  teste;
-
   checkboxView = true;
   botaoDefaultView = true;
   botaoPrimaryView = true;
@@ -39,14 +37,64 @@ export class AppComponent implements AfterViewInit {
   stepperView = true;
   dynamicForView = true;
 
+  fields: Array<any> = [
+    {
+      property: 'name',
+      divider: 'PERSONAL DATA',
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+      gridColumns: 6,
+      gridSmColumns: 12,
+      order: 1,
+      placeholder: 'Type your name',
+      icon: 'po-icon po-icon-user'
+    },
+    { property: 'cpf', label: 'CPF', mask: '999.999.999-99', gridColumns: 6, gridSmColumns: 12, visible: false },
+    { property: 'cnpj', label: 'CNPJ', mask: '99.999.999/9999-99', gridColumns: 6, gridSmColumns: 12, visible: false },
+    { property: 'genre', gridColumns: 6, gridSmColumns: 12, options: ['Male', 'Female', 'Other'], order: 2 },
+    {
+      property: 'shortDescription',
+      label: 'Short Description',
+      gridColumns: 12,
+      gridSmColumns: 12,
+      rows: 3,
+      placeholder: 'Type short description'
+    },
+    { property: 'email', divider: 'CONTACTS', gridColumns: 6, icon: 'po-icon-mail' },
+    { property: 'phone', mask: '(99) 99999-9999', gridColumns: 6, icon: 'po-icon po-icon-telephone' },
+    {
+      property: 'state',
+      gridColumns: 6,
+      options: [
+        { label: 'Santa Catarina', value: 1 },
+        { label: 'São Paulo', value: 2 },
+        { label: 'Rio de Janeiro', value: 3 },
+        { label: 'Minas Gerais', value: 4 }
+      ]
+    },
+    {
+      property: 'wage',
+      type: 'currency',
+      gridColumns: 6,
+      gridSmColumns: 12,
+      decimalsLength: 2,
+      thousandMaxlength: 7,
+      icon: 'po-icon-finance'
+    }
+  ];
+
+  // Cor primária
   brandFormP = this.formBuilder.group({
     colorAction: [null]
   });
 
+  // Cor secundária
   brandFormS = this.formBuilder.group({
     colorAction: [null]
   });
 
+  // Cor terciária
   brandFormT = this.formBuilder.group({
     colorAction: [null]
   });
@@ -56,6 +104,7 @@ export class AppComponent implements AfterViewInit {
     color: [null],
     colorHover: [null],
     borderColor: [null],
+    borderColorHover: [null],
     textColor: [null],
     colorAction: [null],
     textColorHover: [null]
@@ -66,15 +115,15 @@ export class AppComponent implements AfterViewInit {
     color: [null],
     colorHover: [null],
     textColor: [null],
-    textColorHover: [null]
+    textColorHover: [null],
+    colorBackgroundHover: [null]
   });
 
   // Botão Link
   buttonFormLink = this.formBuilder.group({
     color: [null],
     colorHover: [null],
-    textColor: [null],
-    textColorHover: [null]
+    colorBackgroundHover: [null]
   });
 
   // CheckBox
@@ -105,23 +154,28 @@ export class AppComponent implements AfterViewInit {
     colorAction: '--color-tertiary'
   };
 
+  // Variáveis customizaveis
   private readonly formPropertyDictButtonP = {
     color: '--color-button-background-color-primary',
     colorHover: '--color-button-background-color-primary-hover',
     borderColor: '--color-button-border-primary',
-    textColor: '--color-button-color-primary'
+    borderColorHover: '--color-button-border-primary',
+    textColor: '--color-button-color-primary',
+    textColorHover: '--color-button-color-primary-hover'
   };
 
   private readonly formPropertyDictButtonD = {
     color: '--color-button-border',
     colorHover: '--color-button-border-hover',
     textColor: '--color-button-color',
-    textColorHover: '--color-button-color-hover'
+    textColorHover: '--color-button-color-hover',
+    colorBackgroundHover: '--color-button-background-color-hover'
   };
 
   private readonly formPropertyDictButtonL = {
     color: '--color-button-color-link',
-    colorHover: '--color-button-color-link-hover'
+    colorHover: '--color-button-color-link-hover',
+    colorBackgroundHover: '--color-button-background-color-link-hover'
   };
 
   private readonly formPropertyDictCheckbox = {
@@ -140,6 +194,42 @@ export class AppComponent implements AfterViewInit {
 
   openGetcss() {
     this.viewCSSModal.open();
+  }
+
+  resetCss() {
+    this.brandFormP.reset();
+    document.getElementsByTagName('html')[0].style.setProperty('--color-primary', null);
+
+    this.brandFormS.reset();
+    document.getElementsByTagName('html')[0].style.setProperty('--color-secondary', null);
+
+    this.brandFormT.reset();
+    document.getElementsByTagName('html')[0].style.setProperty('--color-tertiary', null);
+
+    this.buttonFormDefault.reset();
+    Object.keys(this.formPropertyDictButtonD).forEach((fieldName: string) => {
+      this.buttonD.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonD[fieldName], null);
+    });
+
+    this.buttonFormPrimary.reset();
+    Object.keys(this.formPropertyDictButtonP).forEach((fieldName: string) => {
+      this.buttonP.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonP[fieldName], null);
+    });
+
+    this.buttonFormLink.reset();
+    Object.keys(this.formPropertyDictButtonL).forEach((fieldName: string) => {
+      this.buttonL.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonL[fieldName], null);
+    });
+
+    this.checkBoxForm.reset();
+    Object.keys(this.formPropertyDictCheckbox).forEach((fieldName: string) => {
+      this.checkBox.checkBoxElement.nativeElement.style.setProperty(this.formPropertyDictCheckbox[fieldName], null);
+    });
+
+    this.switchForm.reset();
+    Object.keys(this.formPropertyDictSwitch).forEach((fieldName: string) => {
+      this.switch.switchContainer.nativeElement.style.setProperty(this.formPropertyDictSwitch[fieldName], null);
+    });
   }
 
   copyToClipboard() {
@@ -204,82 +294,107 @@ export class AppComponent implements AfterViewInit {
   }
 
   private checkChangesButtonP(changes: { [key: string]: string }): void {
-    this.resultButtonP['nativeElement'].innerHTML = '/*button primary*/ <br>po-button {<br>';
+    if (!this.isEmpty(changes)) {
+      this.resultButtonP['nativeElement'].innerHTML = '/*button primary*/ <br>po-button {<br>';
 
-    Object.keys(changes).forEach((fieldName: string) => {
-      const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
+      Object.keys(changes).forEach((fieldName: string) => {
+        const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
 
-      if (changes[fieldName]) {
-        this.buttonP.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonP[fieldName], value);
+        if (changes[fieldName]) {
+          this.buttonP.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonP[fieldName], value);
 
-        this.resultButtonP['nativeElement'].innerHTML += `${this.formPropertyDictButtonP[fieldName]}: ${value};<br>`;
-      }
-    });
+          this.resultButtonP['nativeElement'].innerHTML += `${this.formPropertyDictButtonP[fieldName]}: ${value};<br>`;
+        }
+      });
 
-    this.resultButtonP['nativeElement'].innerHTML += '}<br>';
+      this.resultButtonP['nativeElement'].innerHTML += '}<br>';
+    } else {
+      this.resultButtonP['nativeElement'].innerHTML = '';
+    }
   }
 
   private checkChangesButtonD(changes: { [key: string]: string }): void {
-    this.resultButtonD['nativeElement'].innerHTML = '/*button Default*/ <br>po-button {<br>';
+    if (!this.isEmpty(changes)) {
+      this.resultButtonD['nativeElement'].innerHTML = '/*button Default*/ <br>po-button {<br>';
 
-    Object.keys(changes).forEach((fieldName: string) => {
-      const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
+      Object.keys(changes).forEach((fieldName: string) => {
+        const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
 
-      if (changes[fieldName]) {
-        this.buttonD.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonD[fieldName], value);
+        if (changes[fieldName]) {
+          this.buttonD.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonD[fieldName], value);
 
-        this.resultButtonD['nativeElement'].innerHTML += `${this.formPropertyDictButtonD[fieldName]}: ${value};<br>`;
-      }
-    });
+          this.resultButtonD['nativeElement'].innerHTML += `${this.formPropertyDictButtonD[fieldName]}: ${value};<br>`;
+        }
+      });
 
-    this.resultButtonD['nativeElement'].innerHTML += '}<br>';
+      this.resultButtonD['nativeElement'].innerHTML += '}<br>';
+    } else {
+      this.resultButtonD['nativeElement'].innerHTML = '';
+    }
   }
 
   private checkChangesButtonL(changes: { [key: string]: string }): void {
-    this.resultButtonL['nativeElement'].innerHTML = '/*button Link*/ <br>po-button {<br>';
+    if (!this.isEmpty(changes)) {
+      this.resultButtonL['nativeElement'].innerHTML = '/*button Link*/ <br>po-button {<br>';
 
-    Object.keys(changes).forEach((fieldName: string) => {
-      const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
+      Object.keys(changes).forEach((fieldName: string) => {
+        const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
 
-      if (changes[fieldName]) {
-        this.buttonL.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonL[fieldName], value);
+        if (changes[fieldName]) {
+          this.buttonL.buttonElement.nativeElement.style.setProperty(this.formPropertyDictButtonL[fieldName], value);
 
-        this.resultButtonL['nativeElement'].innerHTML += `${this.formPropertyDictButtonL[fieldName]}: ${value};<br>`;
-      }
-    });
+          this.resultButtonL['nativeElement'].innerHTML += `${this.formPropertyDictButtonL[fieldName]}: ${value};<br>`;
+        }
+      });
 
-    this.resultButtonL['nativeElement'].innerHTML += '}';
+      this.resultButtonL['nativeElement'].innerHTML += '}';
+    } else {
+      this.resultButtonL['nativeElement'].innerHTML = '';
+    }
   }
 
   private checkChangesCheckbox(changes: { [key: string]: string }): void {
-    this.resultCheckBox['nativeElement'].innerHTML = 'po-checkbox {<br>';
+    if (!this.isEmpty(changes)) {
+      this.resultCheckBox['nativeElement'].innerHTML = 'po-checkbox {<br>';
 
-    Object.keys(changes).forEach((fieldName: string) => {
-      const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
+      Object.keys(changes).forEach((fieldName: string) => {
+        const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
 
-      if (changes[fieldName]) {
-        this.checkBox.checkBoxElement.nativeElement.style.setProperty(this.formPropertyDictCheckbox[fieldName], value);
+        if (changes[fieldName]) {
+          this.checkBox.checkBoxElement.nativeElement.style.setProperty(
+            this.formPropertyDictCheckbox[fieldName],
+            value
+          );
 
-        this.resultCheckBox['nativeElement'].innerHTML += `${this.formPropertyDictCheckbox[fieldName]}: ${value};<br>`;
-      }
-    });
-    this.resultCheckBox['nativeElement'].innerHTML += '}';
+          this.resultCheckBox[
+            'nativeElement'
+          ].innerHTML += `${this.formPropertyDictCheckbox[fieldName]}: ${value};<br>`;
+        }
+      });
+      this.resultCheckBox['nativeElement'].innerHTML += '}';
+    } else {
+      this.resultCheckBox['nativeElement'].innerHTML = '';
+    }
   }
 
   private checkChangesSwitch(changes: { [key: string]: string }): void {
-    this.resultSwitch['nativeElement'].innerHTML = 'po-switch {<br>';
+    if (!this.isEmpty(changes)) {
+      this.resultSwitch['nativeElement'].innerHTML = 'po-switch {<br>';
 
-    Object.keys(changes).forEach((fieldName: string) => {
-      const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
+      Object.keys(changes).forEach((fieldName: string) => {
+        const value = /color/i.test(fieldName) ? changes[fieldName] : `var(--${changes[fieldName]})`;
 
-      if (changes[fieldName]) {
-        this.switch.switchContainer.nativeElement.style.setProperty(this.formPropertyDictSwitch[fieldName], value);
+        if (changes[fieldName]) {
+          this.switch.switchContainer.nativeElement.style.setProperty(this.formPropertyDictSwitch[fieldName], value);
 
-        this.resultSwitch['nativeElement'].innerHTML += `${this.formPropertyDictSwitch[fieldName]}: ${value};<br>`;
-      }
-    });
+          this.resultSwitch['nativeElement'].innerHTML += `${this.formPropertyDictSwitch[fieldName]}: ${value};<br>`;
+        }
+      });
 
-    this.resultSwitch['nativeElement'].innerHTML += '}';
+      this.resultSwitch['nativeElement'].innerHTML += '}';
+    } else {
+      this.resultSwitch['nativeElement'].innerHTML = '';
+    }
   }
 
   private checkChanges() {
@@ -292,50 +407,7 @@ export class AppComponent implements AfterViewInit {
     );
   }
 
-  fields: Array<any> = [
-    {
-      property: 'name',
-      divider: 'PERSONAL DATA',
-      required: true,
-      minLength: 4,
-      maxLength: 50,
-      gridColumns: 6,
-      gridSmColumns: 12,
-      order: 1,
-      placeholder: 'Type your name',
-      icon: 'po-icon po-icon-user'
-    },
-    { property: 'cpf', label: 'CPF', mask: '999.999.999-99', gridColumns: 6, gridSmColumns: 12, visible: false },
-    { property: 'cnpj', label: 'CNPJ', mask: '99.999.999/9999-99', gridColumns: 6, gridSmColumns: 12, visible: false },
-    { property: 'genre', gridColumns: 6, gridSmColumns: 12, options: ['Male', 'Female', 'Other'], order: 2 },
-    {
-      property: 'shortDescription',
-      label: 'Short Description',
-      gridColumns: 12,
-      gridSmColumns: 12,
-      rows: 3,
-      placeholder: 'Type short description'
-    },
-    { property: 'email', divider: 'CONTACTS', gridColumns: 6, icon: 'po-icon-mail' },
-    { property: 'phone', mask: '(99) 99999-9999', gridColumns: 6, icon: 'po-icon po-icon-telephone' },
-    {
-      property: 'state',
-      gridColumns: 6,
-      options: [
-        { label: 'Santa Catarina', value: 1 },
-        { label: 'São Paulo', value: 2 },
-        { label: 'Rio de Janeiro', value: 3 },
-        { label: 'Minas Gerais', value: 4 }
-      ]
-    },
-    {
-      property: 'wage',
-      type: 'currency',
-      gridColumns: 6,
-      gridSmColumns: 12,
-      decimalsLength: 2,
-      thousandMaxlength: 7,
-      icon: 'po-icon-finance'
-    }
-  ];
+  private isEmpty(objectVerify) {
+    return Object.values(objectVerify).every(x => x === null || x === '');
+  }
 }
